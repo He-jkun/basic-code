66f8b58f841030a249802dad7380275838f36a8d
macbookair <h441455482@gmail.com>
2022-11-24 09:54:09 +0800

    学习

---
+++ day10-code/src/Test/StringTest5.java
@@ -0,0 +1,26 @@
+package Test;
+
+import java.util.Scanner;
+
+public class StringTest5 {
+    public static void main(String[] args) {
+        /*需求：定义一个方法，实现字符串反转
+        键盘录入一个字符串，调用该方法后，在控制台输出结果
+        例如：键盘录入abc，输出结果cba
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个字符串");
+        String str = sc.next();
+        String s = reversalString(str);
+        System.out.println(s);
+    }
+
+    public static String reversalString(String str) {
+        String s = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char c = str.charAt(i);
+            s = s + c;
+        }
+        return s;
+    }
+}
